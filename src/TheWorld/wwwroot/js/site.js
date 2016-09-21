a7164a2aa360ea10ee1d896a18ffb673a32ba6c2
Alexander James Barr <allybarbar@gmail.com>
2016-09-21 21:23:11 +0100

    Add project files.

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,38 @@
+﻿(function () {
+
+    //var ele = $("#userName")
+    //ele.text = "BBBB"
+
+    //var main = $("#main")
+    //main.on("mouseenter", function () {
+    //    main.style.backgroundColor = "#888";
+    //});
+
+    //main.on("mouseleave", function () {
+    //    main.style.backgroundColor = "";
+
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function()
+    //{
+    //    var me = $(this);
+    //    alert(me.text());
+    //})
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+
+        if( $sidebarAndWrapper.hasClass("hide-sidebar"))
+        {
+            $(this).text("Show");
+        }
+        else {
+            $(this).text("Hide");
+
+        }
+    });
+
+    })();
